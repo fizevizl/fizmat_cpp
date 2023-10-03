9cd086213e29e1c973d769d0689c6911c88fdb8c
fizevizl <dnruban@gmail.com>
2023-10-03 21:24:19 +0200

    lab_1_2

--- lab_01_2.cpp
+++ lab_01_2.cpp
@@ -1,1 +1,13 @@
-dfd rwffe
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    float h, r, result;
+    cout << "h = "; cin >> h;
+    cout << "r = "; cin >> r;
+    result = M_PI * pow(r, 2) * h;
+    cout << result; 
+    return 0;
+}
